906247846f3eed8977261288bba68f3e6d881e05
sudarshanCr7 <106097205+sudarshanCr7@users.noreply.github.com>
2022-05-23 21:01:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function function1() {
+    document.getElementById("audio1").play();
+}
+		function function2() {
+    document.getElementById("audio2").play();
+}
+		function function3() {
+    document.getElementById("audio3").play();
+}
+		function function4() {
+    document.getElementById("audio4").play();
+}
+		function function5() {
+    document.getElementById("audio5").play();
+}
+function stopsound(){
+    document.getElementById("audio1").pause();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+}
